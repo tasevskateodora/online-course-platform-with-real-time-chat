d4da3dbb0fb939ce9cd9627a05c30c14a7d5857f
Teodora Tasevska <teodoratasevska@Mac.home>
2025-10-18 11:57:07 +0200

    Added course categories

--- courses/urls.py
+++ courses/urls.py
@@ -8,15 +8,17 @@ urlpatterns = [
     path('', views.CourseListView.as_view(), name='list'),
     path('category/<int:category_id>/', views.CourseListView.as_view(), name='by_category'),
 
+    # Инструктор функции - ставени пред <slug:slug>/ за да има приоритет
+    path('create/', views.CourseCreateView.as_view(), name='create'),
+    path('my-courses/', views.InstructorCoursesView.as_view(), name='my_courses'),
+
     # Детали и запишување
     path('<slug:slug>/', views.CourseDetailView.as_view(), name='detail'),
     path('<slug:slug>/enroll/', views.EnrollCourseView.as_view(), name='enroll'),
     path('<slug:slug>/lesson/<int:lesson_id>/', views.LessonDetailView.as_view(), name='lesson_detail'),
 
-    # Инструктор функции
-    path('create/', views.CourseCreateView.as_view(), name='create'),
+    # Управување со курс
     path('<slug:slug>/edit/', views.CourseUpdateView.as_view(), name='edit'),
-    path('my-courses/', views.InstructorCoursesView.as_view(), name='my_courses'),
     path('<slug:slug>/manage/', views.CourseManageView.as_view(), name='manage'),
     path('<slug:slug>/lessons/add/', views.LessonCreateView.as_view(), name='add_lesson'),
     path('<slug:slug>/lessons/<int:lesson_id>/edit/', views.LessonUpdateView.as_view(), name='edit_lesson'),
